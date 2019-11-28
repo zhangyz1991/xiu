748964cd50df2f2306bb9238b7dcc76b3f1fb6a4
zyz <yuntop@qq.com>
2019-11-28 15:11:39 +0800

    时间格式化

--- xiu-framework/src/main/java/com/vick/framework/config/LocalDateTimeSerializerConfig.java
+++ xiu-framework/src/main/java/com/vick/framework/config/LocalDateTimeSerializerConfig.java
@@ -3,6 +3,7 @@ package com.vick.framework.config;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,6 +12,7 @@ import java.time.format.DateTimeFormatter;
  * @author zyz
  * @since 2019-11-27
  */
+@Configuration
 public class LocalDateTimeSerializerConfig {
 
     /**
