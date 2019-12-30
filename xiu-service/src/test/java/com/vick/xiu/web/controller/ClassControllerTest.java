e58fcbb46d9a6368bacdbb4707fcb5a080a9fd41
zyz <yuntop@qq.com>
2019-12-31 01:26:32 +0800

    添加测试模块

---
+++ xiu-service/src/test/java/com/vick/xiu/web/controller/ClassControllerTest.java
@@ -0,0 +1,32 @@
+package com.vick.xiu.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ClassControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void list() throws Exception {
+        MvcResult result = mvc.perform(get("/class/list").content("{\"currentPage\":1,\"pageSize\":10}"))
+                .andExpect(status().isOk())// 模拟向testRest发送get请求
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))// 预期返回值的媒体类型text/plain;charset=UTF-8
+                .andReturn();// 返回执行请求的结果
+        System.out.println(result.getResponse().getContentAsString());
+    }
+}
